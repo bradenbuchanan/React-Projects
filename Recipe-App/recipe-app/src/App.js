3c29f024b16c4a190c27bfce4c65c7f4366a3169
Braden Buchanan <bradenbuchanan@ymail.com>
2019-12-19 16:11:45 -0500

    update recipe app

--- Recipe-App/recipe-app/src/App.js
+++ Recipe-App/recipe-app/src/App.js
@@ -7,33 +7,56 @@ const App = () => {
   const APP_KEY = "399d8eb81fa80034626227beded52c07";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDeault();
+    setQuery(search);
+    setSearch("");
+  };
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-name" type="text" />
-        <button className="search-button" type="sumbit"></button>
-      </form>
-      {recipes.map(recipe => (
-        <Recipe
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-name"
+          type="text"
+          value={search}
+          onChange={updateSearch}
         />
-      ))}
+        <button className="search-button" type="sumbit">
+          search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 };
